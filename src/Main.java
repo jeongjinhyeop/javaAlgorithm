ee4c43b56b5d3bbc987ad728525304244dde8a4d
JeongJinHyeop <jhh9619@gmail.com>
2023-06-07 14:31:55 +0900

    6/7 morning algorithm

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,12 @@ import java.util.*;
 
 class Main {
 
-    static int n,total=0;
-    static String answer="NO";
-    boolean flag=false;
-    public void DFS(int L,int sum,int[] arr){
-        if (flag) return;
-        if (sum>total/2) return;
+    static int answer=Integer.MIN_VALUE,c,n;
+    public  void DFS(int L,int sum,int[] arr){
+        for (int i=0; i<arr.length; i++)
+        if(sum>c) return;
         if (L==n){
-            if ((total-sum)==sum){
-                answer="YES";
-                flag=true;
-            }
+            answer=Math.max(answer,sum);
         }
         else{
             DFS(L+1,sum+arr[L],arr);
@@ -29,11 +24,11 @@ class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-         n = kb.nextInt();
+        c= kb.nextInt();
+        n = kb.nextInt();
         int[] arr = new int[n];
         for (int i=0; i<n; i++){
             arr[i]=kb.nextInt();
-            total+=arr[i];
         }
         T.DFS(0,0,arr);
         System.out.println(answer);
--- src/com/solve/Main82.java
+++ src/com/solve/Main82.java
@@ -3,18 +3,15 @@ package com.solve;
 
 import java.util.*;
 
-class Main81 {
-    static int n,total=0;
-    static String answer="NO";
-    boolean flag=false;
-    public void DFS(int L,int sum,int[] arr){
-        if (flag) return;
-        if (sum>total/2) return;
+
+class Main82 {
+
+    static int answer=Integer.MIN_VALUE,c,n;
+    public  void DFS(int L,int sum,int[] arr){
+        for (int i=0; i<arr.length; i++)
+            if(sum>c) return;
         if (L==n){
-            if ((total-sum)==sum){
-                answer="YES";
-                flag=true;
-            }
+            answer=Math.max(answer,sum);
         }
         else{
             DFS(L+1,sum+arr[L],arr);
@@ -23,14 +20,17 @@ class Main81 {
     }
 
 
+
+
+
     public static void main(String[] args) {
-        Main81 T = new Main81();
+        Main82 T = new Main82();
         Scanner kb = new Scanner(System.in);
+        c= kb.nextInt();
         n = kb.nextInt();
         int[] arr = new int[n];
         for (int i=0; i<n; i++){
             arr[i]=kb.nextInt();
-            total+=arr[i];
         }
         T.DFS(0,0,arr);
         System.out.println(answer);
