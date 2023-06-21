5a35829aff51441c1660fd7804932b0d038c821a
JeongJinHyeop <jhh9619@gmail.com>
2023-06-21 18:35:16 +0900

    i modify

--- src/com/solve/Main25.java
+++ src/com/solve/Main25.java
@@ -7,12 +7,10 @@ class Main25 {
     public int solution(int n) {//이중for문으로 가능하긴 한데 시간초과뜰 가능성 높다고 하심
         int answer = 0;  //에라토스테네스 체 방식
         int[] ch= new int[n+1];
-        for (int i =1; i<=n; i++){
+        for (int i =2; i<=n; i++){
             if (ch[i]==0){
                 answer++;
-                for (int j =i; j<=n; j=j+i){
-                    ch[j]=1;
-                }
+                for (int j =i; j<=n; j=j+i) ch[j]=1;
             }
         }
 
